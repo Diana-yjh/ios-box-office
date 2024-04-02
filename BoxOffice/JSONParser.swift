33228624f3a1c45dc80ec2d15b34ca810aaec673
forseaest <u3z@naver.com>
2024-04-02 14:25:57 +0900

    feat: 에셋의 json 파일을 파싱하는 JSONParser의 parse() 메소드 구현

---
+++ BoxOffice/JSONParser.swift
@@ -0,0 +1,22 @@
+//
+//  JSONParser.swift
+//  BoxOffice
+//
+//  Created by Danny, Diana, gama on 4/2/24.
+//
+
+import UIKit
+
+func parse() {
+    let decoder = JSONDecoder()
+    
+    guard let jsonData = NSDataAsset(name: "box_office_sample") else {
+        return
+    }
+    
+    guard let decodedData = try? decoder.decode(DailyBoxOffice.self, from: jsonData.data) else {
+        return
+    }
+    
+    print(decodedData)
+}
