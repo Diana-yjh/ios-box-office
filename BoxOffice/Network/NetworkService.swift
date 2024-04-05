2cb5a00a69f3088d5ad94ce7bf0a477bb19b3e5a
danny <minzami1215@gmail.com>
2024-04-05 13:46:08 +0900

    feat: 영화진흥위원회 API_KEY 값 숨기는 기능 추가 및 gitignore 설정에 *.xccconfig 내용 추가

--- BoxOffice/Network/NetworkService.swift
+++ BoxOffice/Network/NetworkService.swift
@@ -8,12 +8,18 @@
 import Foundation
 
 class NetworkService {
-    let urltoString = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=APIKEY&targetDt=20240101"
-    
     func startLoad() {
+        guard let apiKey = Bundle.main.apiKey else {
+            print("API KEY 값을 로드하지 못했습니다.")
+            return
+        }
+        
+        let urltoString = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(apiKey)&targetDt=20240101"
+        
         guard let url = URL(string: urltoString) else {
             return
         }
+        
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 print(error)
@@ -38,5 +44,11 @@ class NetworkService {
             }
         }
         task.resume()
+    }
+}
+
+extension Bundle {
+    var apiKey: String? {
+        return infoDictionary?["API_KEY"] as? String
     }
 }
