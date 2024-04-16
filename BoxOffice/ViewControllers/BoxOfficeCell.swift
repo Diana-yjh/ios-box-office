b05d4c102d960bc4f8cb87288de7361c8de36f3f
forseaest <u3z@naver.com>
2024-04-16 11:30:27 +0900

    feat: BoxOfficeCell 구현

--- BoxOffice/ViewControllers/BoxOfficeCell.swift
+++ BoxOffice/ViewControllers/BoxOfficeCell.swift
@@ -8,5 +8,137 @@
 import UIKit
 
 class BoxOfficeCell: UICollectionViewCell {
+    static let reuseIdentifier = "boxOfficeCell"
     
+    let cellStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .center
+        
+        return stackView
+    }()
+    
+    let arrowImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.tintColor = .systemGray2
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    let rankStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 2
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        
+        return stackView
+    }()
+    
+    let informationStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 2
+        
+        return stackView
+    }()
+    
+    let rankNumberLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textColor = UIColor.black
+        
+        return label
+    }()
+    
+    let rankChangeLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let audienceLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        configure()
+    }
+    
+    func configure() {
+        contentView.addSubview(cellStackView)
+        [rankStackView, informationStackView, arrowImageView].forEach {
+            self.cellStackView.addArrangedSubview($0)
+        }
+        
+        [rankNumberLabel, rankChangeLabel].forEach {
+            self.rankStackView.addArrangedSubview($0)
+        }
+        
+        [titleLabel, audienceLabel].forEach {
+            self.informationStackView.addArrangedSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            cellStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            cellStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            cellStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            cellStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            rankStackView.widthAnchor.constraint(equalTo: cellStackView.widthAnchor, multiplier: 0.2),
+            rankStackView.centerYAnchor.constraint(equalTo: cellStackView.centerYAnchor),
+            rankStackView.leadingAnchor.constraint(equalTo: cellStackView.leadingAnchor),
+            rankStackView.trailingAnchor.constraint(equalTo: informationStackView.leadingAnchor),
+            
+            informationStackView.centerYAnchor.constraint(equalTo: cellStackView.centerYAnchor),
+            informationStackView.leadingAnchor.constraint(equalTo: rankStackView.trailingAnchor),
+            informationStackView.trailingAnchor.constraint(equalTo: arrowImageView.leadingAnchor),
+            
+            arrowImageView.widthAnchor.constraint(equalTo: cellStackView.widthAnchor, multiplier: 0.15),
+            arrowImageView.centerYAnchor.constraint(equalTo: cellStackView.centerYAnchor),
+            arrowImageView.leadingAnchor.constraint(equalTo: informationStackView.trailingAnchor),
+            arrowImageView.trailingAnchor.constraint(equalTo: cellStackView.trailingAnchor),
+        ])
+    }
 }
--- BoxOffice/ViewControllers/ViewController.swift
+++ BoxOffice/ViewControllers/ViewController.swift
@@ -8,23 +8,35 @@
 import UIKit
 
 struct Temp: Hashable {//이후에 사용 or 수정
-    
+    var movieTitle: String
+    var audience: String
 }
 
 class ViewController: UIViewController {
     private var collectionView: UICollectionView!
     private var dataSource: UICollectionViewDiffableDataSource<Int, Temp>!
-    var tempData: [Temp]?
+    var tempData: [Temp] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = DateFormatter.fetchYesterdayDate(dateFormatType: .navigationTitle)
+        
+        tempData = [
+            Temp(movieTitle: "제목1", audience: "관객수"),
+            Temp(movieTitle: "제목2", audience: "관객수2"),
+            Temp(movieTitle: "제목3", audience: "관객수3"),
+            Temp(movieTitle: "제목4", audience: "관객수4"),
+        ]
+        
+        configureCollectionView()
+        configureDataSource()
+        setSnapshot()
     }
     
     func configureCollectionView() {
         self.collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: self.createCollectionViewLayout())
-        self.collectionView.register(BoxOfficeCell.self, forCellWithReuseIdentifier: "BoxOfficeCell")
+        self.collectionView.register(BoxOfficeCell.self, forCellWithReuseIdentifier: BoxOfficeCell.reuseIdentifier)
         self.view.addSubview(self.collectionView)
     }
     
@@ -38,21 +50,35 @@ extension ViewController {
     func configureDataSource() {
         dataSource = UICollectionViewDiffableDataSource<Int, Temp>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
             
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BoxOfficeCell", for: indexPath) as? BoxOfficeCell else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BoxOfficeCell.reuseIdentifier, for: indexPath) as? BoxOfficeCell else {
+                print("?")
                 return UICollectionViewCell()
             }
             //cell에 데이터 넣어주고 싶으면 cell.parameter = Data 해서 넣어주기~~
+            
+            cell.rankNumberLabel.text = "\(indexPath.row)"
+            cell.rankChangeLabel.text = "순위 변동 라벨"
+            
+            cell.titleLabel.text = "영화 제목"
+            cell.audienceLabel.text = "오늘 Z,ZZZ명 / 총 XX,XXX명"
+            
+            print("\(cell.rankNumberLabel.text)")
+            
             return cell
         })
     }
     
     func setSnapshot() {
-        if dataSource == nil { return } //불필요하지만 처음 설정 때 없으면 에러 나더라구요? ㅎㅎㅎㅎㅎㅎ
+//        if dataSource == nil { return } //불필요하지만 처음 설정 때 없으면 에러 나더라구요? ㅎㅎㅎㅎㅎㅎ\\
         
         var snapShot = NSDiffableDataSourceSnapshot<Int, Temp>()
         let section = Array(0...1) //저희는 근데 섹션 한개쓸거라 ㅋㅋ.. 근데 방법을 모름..
         snapShot.appendSections(section)
-        snapShot.appendItems(tempData!, toSection: 0)//강제옵셔널 수정!!
+////        snapShot.appendItems(tempData!, toSection: 0)//강제옵셔널 수정!!
+        snapShot.appendItems(tempData, toSection: 0)
+        
+//        snapShot.appendSections([0])
+//        snapShot.appendItems(tempData)
         dataSource.apply(snapShot)
     }
 }
