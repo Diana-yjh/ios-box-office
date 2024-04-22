14aa3b53f15ab70a25277c27b7d0cfa037b37c24
forseaest <u3z@naver.com>
2024-04-22 23:35:20 +0900

    feat: 카카오 api와 네트워킹을 위한 KakaoSearchData 모델 구현

---
+++ BoxOffice/Models/KakaoSearchData.swift
@@ -0,0 +1,47 @@
+//
+//  KakaoSearchData.swift
+//  BoxOffice
+//
+//  Created by Danny, Diana, gama on 4/22/24.
+//
+
+import Foundation
+
+struct KakaoSearchData: Codable {
+    let documents: [Document]
+    let meta: Meta
+}
+
+struct Document: Codable {
+    let collection: String
+    let datetime: String
+    let displaySiteName: String
+    let docURL: String
+    let height: Int
+    let imageURL: String
+    let thumbnailURL: String
+    let width: Int
+
+    enum CodingKeys: String, CodingKey {
+        case collection
+        case datetime
+        case displaySiteName = "display_sitename"
+        case docURL = "doc_url"
+        case height
+        case imageURL = "image_url"
+        case thumbnailURL = "thumbnail_url"
+        case width
+    }
+}
+
+struct Meta: Codable {
+    let isEnd: Bool
+    let pageableCount: Int
+    let totalCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case isEnd = "is_end"
+        case pageableCount = "pageable_count"
+        case totalCount = "total_count"
+    }
+}
